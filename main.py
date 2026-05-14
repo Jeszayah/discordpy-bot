31f6209dfcfeef79fcfd7c82c7d3dded35c00123
Jessica <69566248+Jeszayah@users.noreply.github.com>
2026-05-14 01:04:29 -1000

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix='!', intents=intents, status=discord.Status.idle, activity=discord.Game(name="with xen"))
+bot = commands.Bot(command_prefix='!', intents=intents, status=discord.Status.idle, activity=discord.CustomActivity(name="with xen"))
+
 
 
 
